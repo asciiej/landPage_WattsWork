b9efb3241c5d81ee849f0520283eeb13a0856457
KatarineR <131694251+KatarineR@users.noreply.github.com>
2024-12-04 17:52:17 -0300

    Finalizei a seção dados

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Banner from './components/Banner/Banner'; // Caminho para o componente Banner
 import Beneficios from './components/Beneficios/Beneficios';
+import Dados from './components/Dados/Dados';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Banner />
     <Beneficios />
+    <Dados />
   </React.StrictMode>
 );
