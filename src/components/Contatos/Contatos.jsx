1ee7a4b196239df84e9b3ed078336b6fb7ce8516
Rayane <rayanereismota2@gmail.com>
2024-12-19 22:47:30 -0300

    Revisão e adição da Seção Contatos

--- src/components/Contatos/Contatos.jsx
+++ src/components/Contatos/Contatos.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Contatos.module.css';
+import  styles from './Contatos.module.css'
 
 import endereco from '../../img/img-contato/local.png';
 import wpp from '../../img/img-contato/wpp.png';
@@ -8,14 +8,13 @@ import telefone from '../../img/img-contato/telefone.png';
 const Contato = () => {
   return (
     <div className={styles.contato}>
-      <h2 className={styles.titulo}>Ficou alguma dúvida?</h2>
-      <section className={styles.centro}>
+      <h1>Ficou alguma dúvida?</h1>
         <div className={styles.containerContato}>
           {/* Coluna da esquerda (endereço) */}
           <div className={styles.endereco}>
             <div className={styles.enderecoTop}>
-              <img src={endereco} alt="Endereço" className={styles.imagemEndereco} />
-              <h1 className={styles.titulo}>Endereço</h1>
+              <img src={endereco} alt="Endereço"/>
+              <h3>Endereço</h3>
             </div>
             <div className={styles.enderecoBottom}>
               <div className={styles.sedeInfo}>
@@ -30,24 +29,24 @@ const Contato = () => {
 
           {/* Coluna da direita (informações de contato) */}
           <div className={styles.contatoInfo}>
-            <div className={styles.wpp}>
-              <img src={wpp} alt="WhatsApp" className={styles.imagemWpp} />
-              <div className={styles.contatoTexto}>
-                <h1 className={styles.titulo}>WhatsApp</h1>
-                <p className={styles.numero}>(32) 99979-9600 <br /> </p> <p>(21) 99526-0389</p>
+            <div className={styles.contatoCard}>
+              <img src={wpp} alt="WhatsApp"/>
+              <div>
+                <h3>WhatsApp</h3>
+                <p>(32) 99979-9600 <br /> </p> <p>(21) 99526-0389</p>
               </div>
             </div>
-            <div className={styles.telefone}>
-              <img src={telefone} alt="Telefone" className={styles.imagemTelefone} />
-              <div className={styles.contatoTexto}>
-                <h1 className={styles.titulo}>Telefone</h1>
-                <p>(32) 99979-9600 <br /> </p> <p>(21) 99526-0389</p>
+            <div className={styles.contatoCard}>
+              <img src={telefone} alt="Telefone"/>
+              <div>
+                <h3>Telefone</h3>
+                <p>(32) 99979-9600 <br /> </p> <p> (21) 99526-0389</p>
               </div>
             </div>
           </div>
 
         </div>
-      </section>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import Dados from './components/Dados/Dados';
 import NossaHistoria from './components/NossaHistoria/NossaHistoria'
 import NossasSolucoes from './components/NossasSolucoes/NossasSolucoes';
 import Cabecalho from './components/Cabecalho/Cabecalho';
+import Parceiros from './components/Parceiros/Parceiros';
+import Garantias from './components/Garantia/Garantia';
+import Rodape from './components/Rodape/Rodape';
+import Contato from './components/Contatos/Contatos';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,5 +21,9 @@ root.render(
     <NossasSolucoes />
     <Dados />
     <NossaHistoria />
+    <Parceiros />
+    <Garantias />
+    <Contato />
+    <Rodape />
   </React.StrictMode>
 );
